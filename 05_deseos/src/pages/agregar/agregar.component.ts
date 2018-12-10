47e29284ea540907d046d144fd250590eadb5490
Alexánder Arévalo <edwin.arevalo@gmail.com>
2018-12-10 15:49:57 -0500

    ok

--- 05_deseos/src/pages/agregar/agregar.component.ts
+++ 05_deseos/src/pages/agregar/agregar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Lista, ListaItem } from '../../app/clases/index';
 
 @Component({
   selector: 'app-agregar',
@@ -9,7 +10,22 @@ export class AgregarComponent implements OnInit {
   nombreLista: string;
   nombreItem: string;
 
+  items:ListaItem[] = [];
+
   constructor() { }
 
   ngOnInit(): void { }
+
+  agregar() {
+    if( this.nombreItem.length == 0 ) {
+      return;
+    }
+
+    let item = new ListaItem();
+    item.nombre = this.nombreItem;
+
+    this.items.push( item );
+    this.nombreItem = "";
+
+  }
 }
